0a9001a655c6ccd20888ddb2c7e1091b9101de1e
sakibhasansufi <sakibhasansufi@gmail.com>
2024-04-30 20:09:35 +0600

    data delete operation added

--- src/Pages/MyItem.jsx
+++ src/Pages/MyItem.jsx
@@ -3,6 +3,11 @@ import { Link, useLoaderData } from "react-router-dom";
 
 const MyItem = () => {
     const arts = useLoaderData();
+    // const {image, item, subCategory, description, price, rating, customization, process, stock, email, name} = arts;
+    const handleDelete = _id => {
+        console.log(_id);
+
+    }
     return (
         <div>
             <Helmet>
@@ -10,28 +15,29 @@ const MyItem = () => {
             </Helmet>
             {
                 arts.map(art => <div key={art._id} className="flex justify-center">
-                    <section className="grid grid-cols-1 md:grid-cols-2">
+                    
                         <div className="card w-96 bg-base-100 shadow-xl">
                             <figure className="px-10 pt-10">
-                                <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+                                <img src={art.image} alt="Shoes" className="rounded-xl" />
                             </figure>
                             <div className="card-body items-center text-center">
-                                <h2 className="card-title">Shoes!</h2>
+                                <h2 className="card-title">Title :{art.item}</h2>
                                 <div className="flex gap-36">
-                                    <h1>Price</h1>
-                                    <h2>Rating</h2>
+                                    <h1>Price : ${art.price}</h1>
+                                    <h2>Rating :{art.rating}</h2>
                                 </div>
                                 <div className="flex mt-2 mb-3 gap-24">
-                                    <h1>Customization</h1>
-                                    <h2>Stock Status</h2>
+                                    <h1>Customization :{art.customization}</h1>
+                                    <h2>Stock Status : {art.stock}</h2>
                                 </div>
                                 <div className="card-actions">
                                     <Link to='/update'><button className="btn btn-primary">Update</button></Link>
-                                    <button className="btn btn-error">Delete</button>
+                                    <button onClick={()=>handleDelete(art._id)} className="btn btn-error">Delete</button>
                                 </div>
                             </div>
                         </div>
-                    </section>
+
+                    
                 </div>)
             }
 
